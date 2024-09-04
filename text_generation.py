99a3e44c79173bad8c748024124f785ec13e93c6
Harshal Pawar <datadrivenharshal@gmail.com>
2024-09-04 20:20:40 +0530

    forth commit

--- text_generation.py
+++ text_generation.py
@@ -2,7 +2,9 @@ from transformers import pipeline
 
 def generate_text(objects):
     """Generates descriptive text based on detected objects."""
-    text_generator = pipeline('text-generation', model='gpt-2')
-    prompt = f"In the image, I see {', '.join(objects)}."
+    #text_generator = pipeline('text-generation', model='gpt-2')
+    text_generator = pipeline('text-generation', model='gpt2', use_auth_token='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')
+
+    prompt = f"In the image, I can see {', '.join(objects)}."
     generated_text = text_generator(prompt, max_length=50)[0]['generated_text']
     return generated_text
